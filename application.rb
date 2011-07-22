265aea9b73589cb416a55f9801433651641854a9
Dave Perrett <dave@recurser.com>
2011-07-22 13:45:44 +1200

    Basic error code generation.

--- application.rb
+++ application.rb
@@ -2,17 +2,27 @@ require 'sinatra'
 require 'haml'
 require 'sass'
 
+HTTP_CODES = [400, 401, 403, 404, 410, 500, 501, 503]
+
 set :haml, :format => :html5
 
 get '/' do
+  @http_codes = HTTP_CODES
   haml :index
 end
 
-get '/:path' do
-  haml params[:path].to_sym
+get '/:http_code' do
+  @http_code = params[:http_code].to_i
+  if HTTP_CODES.include? @http_code
+    status @http_code
+    haml :http_code
+  else
+    redirect '/'
+  end
 end
 
-get '/style.css' do
+get '/assets/css/style.css' do
   content_type 'text/css', :charset => 'utf-8'
-  scss :style
+  sass :style
 end
+
